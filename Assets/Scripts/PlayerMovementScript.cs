9103d665ed8908010ee7d8c113350f3c649a2495
snapeeek <maciekwozniak97@gmail.com>
2020-12-13 11:42:19 +0100

    Added player footsteps

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -6,14 +6,29 @@ public class PlayerMovementScript : MonoBehaviour
 {
 	public float moveSpeed = 5f;
 	public Rigidbody2D rb;
+	public AudioSource audioControl;
 
 	Vector2 movement;
+	bool isMoving = false;
 
     // Update is called once per frame
     void Update()
     {
 		movement.x = Input.GetAxisRaw("Horizontal");
 		movement.y = Input.GetAxisRaw("Vertical");
+
+		if (movement.x != 0 || movement.y != 0)
+			isMoving = true;
+		else
+			isMoving = false;
+
+		if (isMoving)
+		{
+			if (!audioControl.isPlaying)
+			audioControl.Play();
+		}
+		else
+			audioControl.Stop();
 	}
 
 	void FixedUpdate()
